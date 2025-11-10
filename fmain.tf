949956680ec034ed38777fe9165ce9865cdb8c44
SadiqRayeen <2005050100032sadiq@gmail.com>
2025-11-10 23:29:45 +0530

    Create fmain.tf

---
+++ fmain.tf
@@ -0,0 +1,23 @@
+apiVersion: v1
+kind: PersistentVolume
+metadata:
+  name: nginx-pv-0
+spec:
+  capacity:
+    storage: 50Gi
+  accessModes:
+    - ReadWriteOnce
+  hostPath:
+    path: /data/nginx/pv0
+---
+apiVersion: v1
+kind: PersistentVolume
+metadata:
+  name: nginx-pv-1
+spec:
+  capacity:
+    storage: 1Gi
+  accessModes:
+    - ReadWriteOnce
+  hostPath:
+    path: /data/nginx/pv1
